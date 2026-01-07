bceff8f36d75d8de516312200053fee91717f4cd
mabab2000 <88932706+mabab2000@users.noreply.github.com>
2026-01-07 11:36:23 +0200

    fix versions issue

--- types.ts
+++ types.ts
@@ -1,27 +1,9 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './screens/HomeScreen';
-import DetailScreen from './screens/DetailScreen';
-import { RootStackParamList } from './types';
-
-const Stack = createNativeStackNavigator<RootStackParamList>();
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: '🌱 Menya Leaf AI' }}
-        />
-        <Stack.Screen
-          name="Detail"
-          component={DetailScreen}
-          options={{ title: 'Plant Details' }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+export type RootStackParamList = {
+  Home: undefined;
+  Detail: {
+    plant: {
+      name: string;
+      info: string;
+    };
+  };
+};
